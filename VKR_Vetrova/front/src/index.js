e6199274d1cb0c344986133167b0c5da66316bf8
Egor <russianfracture@gmail.com>
2023-04-24 11:51:58 +0300

    sync diplome

---
+++ VKR_Vetrova/front/src/index.js
@@ -0,0 +1,27 @@
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Store from './store/store';
+import {BrowserRouter} from 'react-router-dom'
+
+
+const store = new Store();
+
+export const Context = createContext({
+  store,
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Context.Provider value = {{
+    store
+  }}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </Context.Provider>
+);
+
+
